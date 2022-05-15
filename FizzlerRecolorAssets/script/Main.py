cd745f96057d6bd4ef527bdaf660ece58e13d50f
ArengDev <kktdell.1@gmail.com>
2022-05-15 21:42:22 +0700

    Added support for Absolute Fizzlers
    I will be adding support for more beemod fizzlers in this commit it is the Absolute Fizzler. They only work on your
    new maps only due to beemod packing files

--- FizzlerRecolorAssets/script/Main.py
+++ FizzlerRecolorAssets/script/Main.py
@@ -1,10 +1,3 @@
-from base64 import decode
-from codecs import encode
-from errno import EDEADLK
-from signal import pause
-from tracemalloc import stop
-import vpk
-import glob
 import os
 import shutil
 import random
@@ -14,7 +7,12 @@ import time
 var1 = input("Is the assets in the portal 2 directory? (yes/no): ")
 if var1 == "yes":
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    dlcfolderint = int(input("What is your highest dlc folder? (interger) "))
+    print ("1. Fizzler")
+    print ("2. Absolute Fizzler (On your maps ONLY)")
+    EditWhat = int(input("What fizzler are we editing? (Integer) "))
+    print ()
+    dlcfolderint = int(input("What is your highest dlc folder? (Integer) "))
+    print ()
     Color = input("RGB Color code (r for random),(Red: Green: Blue:): ")
     if Color == "r":
         Random1 = random.randint(0,255)
@@ -25,7 +23,7 @@ if var1 == "yes":
     dlcfolder = "portal2_dlc" + str(dlcfolderint)
     #Gets information about the game dlcs
     def NormalFiz():
-        def editfilesmodern(FileName):
+        def editfiles(FileName):
             Directory = "C:/Program Files (x86)/Steam/steamapps/common/Portal 2/" + dlcfolder + "/pak01_dir/materials/effects/"
             FileName2 = Directory + FileName + ".vmt"
             reading_file = open(FileName2,'r')
@@ -93,22 +91,74 @@ if var1 == "yes":
             print ("If you have any questions dm Areng#0001 on discord.")
             time.sleep(2.5)
             print ("You may now close this window.")
-            time.sleep(999999999999999999999)
+            time.sleep(99999999999)
             #End Script
         MakeFolders()
         MoveAssets()
-        editfilesmodern("fizzler")
-        editfilesmodern("fizzler_center")
-        editfilesmodern("fizzler_l")
-        editfilesmodern("fizzler_r")
-        editfilesmodern("fizzler_underground")
-        editfilesmodern("fizzler_underground_elevator")
-        editfilesmodern("fizzler_underground_side_emitters")
-        editfilesmodern("fizzler_underground_wide_center")
-        editfilesmodern("fizzler_underground_wide_side_l")
-        editfilesmodern("fizzler_underground_wide_side_r")
+        editfiles("fizzler")
+        editfiles("fizzler_center")
+        editfiles("fizzler_l")
+        editfiles("fizzler_r")
+        editfiles("fizzler_underground")
+        editfiles("fizzler_underground_elevator")
+        editfiles("fizzler_underground_side_emitters")
+        editfiles("fizzler_underground_wide_center")
+        editfiles("fizzler_underground_wide_side_l")
+        editfiles("fizzler_underground_wide_side_r")
         Packer()
-    NormalFiz()
+    def ABSFizzler():
+        def editfiles(FileName):
+            Directory = "C:/Program Files (x86)/Steam/steamapps/common/Portal 2/bee2/materials/bee2/fizz/abs_fizz/"
+            FileName2 = Directory + FileName + ".vmt"
+            reading_file = open(FileName2,'r')
+            new_file_content = ""
+            for line in reading_file:
+                stripped_line = line.strip()
+                new_line = stripped_line.replace('$FLOW_COLOR "[0.05 0.4 0.6]"','$FLOW_COLOR "{' + Color + '}"')
+                new_file_content += new_line +"\n"
+            reading_file.close()
+            writing_file = open(FileName2,'w')
+            writing_file.write(new_file_content)
+            writing_file.close()
+            reading_file = open(FileName2,'r')
+            new_file_content = ""
+            for line in reading_file:
+                stripped_line = line.strip()
+                new_line = stripped_line.replace('$FLOW_VORTEX_COLOR "[0.4 3.2 4.8]"','$FLOW_VORTEX_COLOR "{' + Color + '}"')
+                new_file_content += new_line +"\n"
+            reading_file.close()
+            writing_file = open(FileName2,'w')
+            writing_file.write(new_file_content)
+            writing_file.close()
+            print ("Successfully edited colors for " + FileName)
+            #Writes the vmt
+        def MoveAssets():
+            source_folder = "C:/Program Files (x86)/Steam/steamapps/common/Portal 2/FizzlerRecolorAssets/effects/Absolute/"
+            destination_folder = "C:/Program Files (x86)/Steam/steamapps/common/Portal 2/bee2/materials/bee2/fizz/abs_fizz/"
+            files_to_move = ['abs_fizz_bounds.vtf','abs_fizz_bounds_l.vtf','abs_fizz_bounds_r.vtf','absolute_center.vmt','absolute_field.vmt','absolute_left.vmt','absolute_right.vmt','retro_absolute_center.vmt','retro_absolute_field.vmt','retro_absolute_field_hallway.vmt','retro_absolute_left.vmt','retro_absolute_right.vmt',]
+            for file in files_to_move:
+                source = source_folder + file
+                destination = destination_folder + file
+                shutil.copyfile(source, destination)
+                print('Copied', file, "to", destination_folder)
+        def Packer():
+            print ()
+            print ()
+            print ()
+            print ("All functions have been completed!")
+            print ("Relaunch Portal2 and it will show the absolute fizzler recolored!")
+            print ("If you have any questions dm Areng#0001 on discord.")
+            time.sleep(2.5)
+            print ("You may now close this window.")
+            time.sleep(99999999999)
+        MoveAssets()
+        editfiles("absolute_field")
+        Packer()
+    if EditWhat == 1:
+        NormalFiz()
+    if EditWhat == 2:
+        ABSFizzler()
+
         
 else: 
     quit
